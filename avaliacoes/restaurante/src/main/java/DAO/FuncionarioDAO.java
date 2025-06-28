c3f4957922fa28e7567659532309be2bb83a0f1c
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-06-28 16:23:52 -0300

    feat: add getNexId() - Funcionario

--- avaliacoes/restaurante/src/main/java/DAO/FuncionarioDAO.java
+++ avaliacoes/restaurante/src/main/java/DAO/FuncionarioDAO.java
@@ -1,5 +1,31 @@
 package DAO;
 
+import java.util.ArrayList;
+
+import models.Funcionario;
+import models.Item;
+
 public class FuncionarioDAO {
+ 
+    public String path = "c:\\Users\\Marco\\Documents\\programming\\java\\cefet\\java-exercicios\\avaliacoes\\restaurante\\src\\main\\java\\db\\item.txt";
     
+    public void add (Funcionario funcionario){
+        
+        int id = this.getNextId();
+    }
+
+    private int getNextId() {
+
+        int maxId = 0;
+        ArrayList<Item> items = getAll();
+
+        for (Item i : items ) {
+            if (i.getId() > maxId) {
+                maxId = i.getId();
+            }
+        }
+
+        return maxId + 1;
+    }
+
 }
